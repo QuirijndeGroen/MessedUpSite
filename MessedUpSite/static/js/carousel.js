c1162663a916c4b6f4e2334dbc4e2c7a9e85fdb7
QuirijndeGroen <qtdegroen@gmail.com>
2026-04-20 21:42:15 +0200

    0.1.1 Refactor carousel structure and styles; update image assets and remove unused color.css; Added auto rotate for carousels

--- MessedUpSite/static/js/carousel.js
+++ MessedUpSite/static/js/carousel.js
@@ -1,5 +1,5 @@
 document.querySelectorAll(".carousel").forEach(carousel => {
-  const items = carousel.querySelectorAll(".card-carousel");
+  const items = carousel.querySelectorAll(".item");
   const buttonsHtml = Array.from(items, () => {
     return `<span class="carousel-button"></span>`;
   });
@@ -12,18 +12,35 @@ document.querySelectorAll(".carousel").forEach(carousel => {
 
     const buttons = carousel.querySelectorAll(".carousel-button");
 
+    let currentIndex = 0;
+    let intervalId;
+
+    function selectItem(index) {
+      items.forEach(item => item.classList.remove("carousel-selected"));
+      buttons.forEach(button => button.classList.remove("button-selected"));
+
+      items[index].classList.add("carousel-selected");
+      buttons[index].classList.add("button-selected");
+      currentIndex = index;
+    }
+
+    function startRotation() {
+      intervalId = setInterval(() => {
+        const nextIndex = (currentIndex + 1) % items.length;
+        selectItem(nextIndex);
+      }, 10000);
+    }
+
     buttons.forEach((button, i) => {
       button.addEventListener("click", () => {
-        items.forEach(item => item.classList.remove("carousel-selected"));
-        buttons.forEach(button => button.classList.remove("button-selected")); 
-
-        items[i].classList.add("carousel-selected");
-        button.classList.add("button-selected");
+        clearInterval(intervalId);
+        selectItem(i);
+        startRotation();
       });
     });
 
-  items[0].classList.add("carousel-selected");
-  buttons[0].classList.add("button-selected");
+  selectItem(0);
+  startRotation();
 
 });
 
